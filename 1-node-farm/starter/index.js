87d6d195c2076ea1b37f14428c2a1dba81f8e602
smf <smf@my-computer.fritz.box>
2019-12-31 17:31:00 +0100

    built some templates 2

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -38,18 +38,58 @@ console.log('Will read file!');
 //////////////////////////////////
 // SERVER
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`);
-const dataObject = JSON.parse(data);
+const replaceTemplate = function(temp, product) {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-
+  // console.log(req.url);
   const pathName = req.url;
 
+  // Overview page
   if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the OVERVIEW');
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el));
+    console.log(cardsHtml);
+
+    res.end(tempOverview);
+
+    // Product page
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT');
+
+    // API
   } else if (pathName === '/api') {
     // Async handling of API <- inefficient; put at top; make synchronous to only have it execute once at the beginning of the script
 
@@ -65,6 +105,8 @@ const server = http.createServer((req, res) => {
       'Content-type': 'application/json'
     });
     res.end(data);
+
+    // Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
