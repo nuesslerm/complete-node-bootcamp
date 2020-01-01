687cd03c08ae40ae2a4642a8a656c3df92ceefa6
smf <smf@my-computer.fritz.box>
2020-01-01 22:38:58 +0100

    nodeJS event loop practice 2

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+const crypto = require('crypto');
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 5;
 
 setTimeout(() => console.log('Timer 1 finished'), 0);
 setImmediate(() => console.log('Immediate 1 finished'));
@@ -13,6 +17,28 @@ fs.readFile('test-file.txt', () => {
   // setImmediate is executed first because nodeJS waits in the polling phase of the event loop until another timer expires; but setImmediate events are executed immediately during the polling wait phase
 
   process.nextTick(() => console.log('Process.nextTick'));
+
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, '"Password encrypted"');
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, '"Password encrypted"');
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, '"Password encrypted"');
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, '"Password encrypted"');
+
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>
+    console.log(Date.now() - start, '"Password encrypted"')
+  );
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>
+    console.log(Date.now() - start, '"Password encrypted"')
+  );
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>
+    console.log(Date.now() - start, '"Password encrypted"')
+  );
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () =>
+    console.log(Date.now() - start, '"Password encrypted"')
+  );
 });
 
 console.log('Hello from the top-level code');
