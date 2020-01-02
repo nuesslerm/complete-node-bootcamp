476983063c81191daa3bc60f9b4088658e762f1b
smf <smf@my-computer.fritz.box>
2020-01-02 02:55:56 +0100

    async 1

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,12 +1,19 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) => {
   console.log(`Breed: ${data}`);
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, result) => {
-      console.log(result.body);
+      // return will break the function right away
+      if (err) return console.log(err.message);
+      console.log(result.body.message);
+
+      fs.writeFile('dog-img.txt', result.body.message, 'utf8', err => {
+        if (err) return console.log(err.message);
+        console.log('Random dog image saved to file!');
+      });
     });
 });
