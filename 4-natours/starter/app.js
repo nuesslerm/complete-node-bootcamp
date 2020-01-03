4be4fe109cd3aeb9045ae17135d71070702ef3ed
smf <smf@my-computer.fritz.box>
2020-01-03 03:09:04 +0100

    refactored request handling and routing

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,65 +3,29 @@ const express = require('express');
 
 const app = express();
 
-// middleware stands between request and response; alters the request object to eg. include data from body
 app.use(express.json());
-
-// http method: get request
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   // 200 is the default port
-//   res.send('You can post to this endpoint...');
-// });
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// HTTP request: GET
-// resource: tours
-app.get('/api/v1/tours', (req, res) => {
-  // this callback function is referred to as the route handler
-
+// separated handler function of the route
+const getAllTours = (req, res) => {
   res.status(200).json({
-    // uses JSend res formatting specifications
     status: 'success',
-    results: tours.length, // also send this when you are responding with an array
+    results: tours.length,
     data: {
-      // name of resource and endpoint is tours; data argument is contained in tours variable, ie. tours: tours
-
-      // ES6 convention: when key (property) and value (data variable) have the same name you only need to write it once
-
       tours
     }
   });
-});
+};
 
-/*
-// responding to URL parameters
-optional parameters
-app.get('/api/v1/tours/:id/:x?/:y?', (req, res) => {
-  console.log(req.params);
-  res.status(200).json({
-    status: 'success'
-  });
-});
-*/
-
-// HTTP request: GET
-// resource: tours
-// responding to URL parameters within tours resource
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
 
   const tour = tours.find(el => el.id === id);
 
-  // if (id > tours.length) {
   if (!tour) {
     return res.status(404).json({
       status: 'fail',
@@ -75,14 +39,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour
     }
   });
-});
+};
 
-// HTTP request: POST
-// resource: tours
-app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body);
-  // we always need to send something (even just a placeholder) to finish the request/response cycle
-
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -92,7 +51,6 @@ app.post('/api/v1/tours', (req, res) => {
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     err => {
-      // 201 = (object) created
       res.status(201).json({
         status: 'success',
         data: {
@@ -101,11 +59,9 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
-// HTTP request: PATCH
-// resource: tours
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -119,11 +75,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '<Updated tour here...>'
     }
   });
-});
+};
 
-// HTTP request: DELETE
-// resource: tours
-app.delete('/api/v1/tour/:id', (req, res) => {
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -136,7 +90,34 @@ app.delete('/api/v1/tour/:id', (req, res) => {
     status: 'success',
     data: null
   });
-});
+};
+
+// HTTP request: GET
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+
+// HTTP request: POST
+// app.post('/api/v1/tours', createTour);
+
+// HTTP request: PATCH
+// app.patch('/api/v1/tours/:id', updateTour);
+
+// HTTP request: DELETE
+// app.delete('/api/v1/tour/:id', deleteTour);
+
+// specified the actions for both routes
+// route: '/api/v1/tours'
+app
+  .route('/api/v1/tours')
+  .get(getAllTours)
+  .post(createTour);
+
+// route: '/api/v1/tour/:id'
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
