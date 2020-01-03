ad23db02f4aee7e0e4964706b31563850468295a
smf <smf@my-computer.fritz.box>
2020-01-03 02:22:49 +0100

    responding to URL parameters

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,6 +3,9 @@ const express = require('express');
 
 const app = express();
 
+// middleware stands between request and response; alters the request object to eg. include data from body
+app.use(express.json());
+
 // http method: get request
 // app.get('/', (req, res) => {
 //   res
@@ -30,9 +33,71 @@ app.get('/api/v1/tours', (req, res) => {
     results: tours.length, // also send this when you are responding with an array
     data: {
       // name of resource and endpoint is tours; data argument is contained in tours variable, ie. tours: tours
-      tours // ES6 convention: when key (property) and value (data variable) have the same name you only need to write it once
+
+      // ES6 convention: when key (property) and value (data variable) have the same name you only need to write it once
+
+      tours
     }
   });
+});
+
+/*
+// responding to URL parameters
+optional parameters
+app.get('/api/v1/tours/:id/:x?/:y?', (req, res) => {
+  console.log(req.params);
+  res.status(200).json({
+    status: 'success'
+  });
+});
+*/
+
+// responding to URL parameters
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+
+  const tour = tours.find(el => el.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
+});
+
+// HTTP request: POST
+// resource: tours
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  // we always need to send something (even just a placeholder) to finish the request/response cycle
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 
 const port = 3000;
