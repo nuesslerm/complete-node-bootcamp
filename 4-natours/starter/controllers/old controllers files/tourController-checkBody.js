ef1500e3575d8294339f350e70d613e516ef0653
smf <smf@my-computer.local>
2020-01-05 19:11:22 +0100

    eslintrc.json

--- 4-natours/starter/controllers/old controllers files/tourController-checkBody.js
+++ 4-natours/starter/controllers/old controllers files/tourController-checkBody.js
@@ -68,7 +68,7 @@ exports.getTour = (req, res) => {
 exports.createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
 
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newTour = Object.aasign({ id: newId }, req.body);
 
   tours.push(newTour);
 
